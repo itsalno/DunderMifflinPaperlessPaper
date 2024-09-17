f35101312230a4ace5644f0f9f16f66e374736fa
alekss <anoviconoks@gmail.com>
2024-09-17 18:09:17 +0200

    Added references between layers

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fd30eff927b17bad7aa510e5629f7034c91562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfda578e1ab25feac8b4e6708fff1670632818b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
