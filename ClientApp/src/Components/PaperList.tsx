6d7f9fb4e2370a8baa1ffde5ee35555a955718dc
alekss <anoviconoks@gmail.com>
2024-09-24 14:50:50 +0200

    Deleting works now

--- ClientApp/src/Components/PaperList.tsx
+++ ClientApp/src/Components/PaperList.tsx
@@ -2,11 +2,14 @@ import {useEffect, useState } from "react";
 import useInitializedData from "../useInitializedData";
 import { useAtom } from "jotai";
 import { PapersAtom } from "../Atoms/PapersAtom";
+import { http } from "../http";
+import toast from "react-hot-toast";
 
 
 function PaperList(){
 
     const [papers] = useAtom(PapersAtom);
+    const [, setPapers] = useAtom(PapersAtom);
 
 
     useInitializedData();
@@ -14,6 +17,29 @@ function PaperList(){
 
 
 
+
+    async function CreatePaper() {
+
+    }
+
+    
+    async function DeletePaper(id) {
+        http.api.papersDeletePaperDelete(id).then(() => {
+            toast.success('Paper deleted');
+            setPapers(papers.filter(p => p.id !== id));
+        }).catch(e => {
+            console.log(e)
+        })
+        
+    }
+
+    
+    async function UpdatePaper() {
+        
+    }
+    
+
+    
 
     return (
         <div className="flex items-left justify-top h-screen p-10">
@@ -25,12 +51,14 @@ function PaperList(){
                         <p className="mt-2">Stock: {paper.stock}</p>
                         <p className="mt-2">Price: {paper.price}</p>
                         <p className="mt-2">Discontinued: {paper.discontinued ? 'yes' : 'no'}</p>
+                        <button onClick={() => DeletePaper(paper.id)}
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full mt-2">Delete
+                        </button>
                     </div>
                 ))}
             </div>
         </div>
     );
-
 
 }
 
